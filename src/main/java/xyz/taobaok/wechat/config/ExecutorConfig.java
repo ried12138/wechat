8faeb1d335373fb4598f9b1abc081ec83c92668d
liuweiran <liuweiran1238@teddymobile.cn>
2021-04-23 10:18:06 +0800

    1、mysql支持

--- src/main/java/xyz/taobaok/wechat/config/ExecutorConfig.java
+++ src/main/java/xyz/taobaok/wechat/config/ExecutorConfig.java
@@ -1,26 +1,26 @@
-package xyz.taobaok.wechat.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
-
-@Configuration
-@EnableAsync
-public class ExecutorConfig {
-
-    @Bean
-    public Executor taskScheduler() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setThreadNamePrefix("order-schedule2-");   // 线程池名前缀
-        executor.setMaxPoolSize(10);    // 最大线程数
-        executor.setCorePoolSize(5);   // 核心线程数（默认线程数）
-        executor.setQueueCapacity(5);   // 缓冲队列数
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        executor.initialize();
-        return executor;
-    }
-}
+//package xyz.taobaok.wechat.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//
+//import java.util.concurrent.Executor;
+//import java.util.concurrent.ThreadPoolExecutor;
+//
+//@Configuration
+//@EnableAsync
+//public class ExecutorConfig {
+//
+//    @Bean
+//    public Executor taskScheduler() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setThreadNamePrefix("order-schedule2-");   // 线程池名前缀
+//        executor.setMaxPoolSize(10);    // 最大线程数
+//        executor.setCorePoolSize(5);   // 核心线程数（默认线程数）
+//        executor.setQueueCapacity(5);   // 缓冲队列数
+//        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+//        executor.initialize();
+//        return executor;
+//    }
+//}
